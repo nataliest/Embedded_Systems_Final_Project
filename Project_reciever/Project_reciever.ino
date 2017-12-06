5b932e9c49c2370a7e0a202afe45879e164037d9
williambrown4110 <32795919+williambrown4110@users.noreply.github.com>
2017-12-05 21:51:00 -0500

    code update

--- Project_reciever/Project_reciever.ino
+++ Project_reciever/Project_reciever.ino
@@ -6,6 +6,7 @@
 
 //Globals as set up in the "gettingstarted" radio program
 bool radioNumber = 0; //random number tbd actual assuming even number is reciever
+bool role = 0;
 RF24 radio(11,12); //SPI pin bus 11,12 (MOSI/MISO) may need to be changed
 byte addresses[][6] = {"1Node","2Node"}; //this is left as is from code may need change
 
@@ -13,17 +14,12 @@ byte addresses[][6] = {"1Node","2Node"}; //this is left as is from code may need
 
 void setup() {
   // put your setup code here, to run once:
-  //TCCR1A = 0b11110011;  //maybe 11110011
-  //TCCR1B = 0b00001100;
-  TCCR0A=0b10100011;
-  TCCR0B=0b00000101;
-  /*setting timer as 10bit with 256prescaler this will give us
-   * 61Hz and will allow for 20+ speed options
-   */
+  TCCR0A = 0b10100011;
+  TCCR0B = 0b00000101;
   DDRD = 0b01100000; //pwm will run off D pins (timer should be connected to D and B ports)
   PORTD = 0b01100000;
-  DDRB=0b00111111;
-  PORTB=0b00101110; //SCK, MOSI, CSN, CE, and IRQ are set to output here
+  DDRB = 0b00111111;
+  PORTB = 0b00101110; //SCK, MOSI, CSN, CE, and IRQ are set to output here
   
   radio.begin(); //turns radio on
 
@@ -49,15 +45,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly
-  int total=255;
-  int totalforward=255; //from accel
-  int totalback = 255;
-  int turn=0;  //from steering mechanism
+  int total=255; //from accel
+  int turn;  //from steering mechanism
   int left;  //holds value to reduce left motor
   int right; //hold value to reduce right motor
+ // int righttotal; //this will be used if the motors do not match in speed
+  //int lefttotal; //see above
   /* 
-   *  OCR0A and OCR0B will be a value between a max value of 125
-   *  and a min value of 97, this will be sent via hex (we might have to make this int
+   *  OCR0A and OCR0B will be a value between a max value of 31
+   *  and a min value of 24, this will be sent via hex (we might have to make this int
    *  I assume that
    *  Pin 5 will be left (OCRB), and Pin 6 will be right (OCRA) motors
   */
@@ -88,8 +84,7 @@ void loop() {
    *  radio.stopListening();
    */
 
-  totalforward = map(total, 0 , 255, 24, 31); //sets the overall motorspeed to a value between 97-125 which are min and max
-  totalback = map(total, 0, 255, 15, 23);
+  total = map(total, 0 , 255, 24, 31);
 
   if ( turn == 0){ //assuming that we are going straight
     left = 0;
@@ -106,7 +101,7 @@ void loop() {
     left = 0;
   }
   
-  OCR0A = (totalforward - right); //if right is <0 this will reduce the right wheel speed
-  OCR0B = (totalback + left); //if left is <0 this will reduce left wheel speed
+  OCR0A = (total - right); //if right is <0 this will reduce the right wheel speed
+  OCR0B = (total - left); //if left is <0 this will reduce left wheel speed
 
 }
